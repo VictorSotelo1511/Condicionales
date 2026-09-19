f0411a45ca10b1c3ba4937cee4192acdbe809771
VictorSotelo1511 <victorsotelo1511@gmail.com>
2026-09-19 15:23:23 -0600

    Fix logic to print the largest of three numbers

--- src/Programa3.java
+++ src/Programa3.java
@@ -15,6 +15,8 @@ public class Programa3 {
         if (num1 > num2) {
            if (num1 > num3){
                 System.out.println(num1);
+           } else {
+                System.out.println(num3);
            }
         } else if (num2 > num3) {
             System.out.print(num2);
