5c74d171bfc9d9f637180555863779f0483d8213
VictorSotelo1511 <victorsotelo1511@gmail.com>
2026-09-19 15:22:14 -0600

    Improve age input handling and output messages
    Refactor age validation logic and update prompts.

--- src/Programa1.java
+++ src/Programa1.java
@@ -4,16 +4,20 @@ public class Programa1 {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingresa tu edad");
+        System.out.print("Ingresa tu edad: ");
         int edad = sc.nextInt();
         sc.nextLine();
 
-        if (edad < 18 && edad > 0) {
-            System.out.println("Menor de Edad");
-        } else if (edad > 18 && edad < 50) {
+        if(edad > 0) {
+            if (edad < 18 && edad > 0) {
+                System.out.println("Menor de Edad");
+            } else if (edad >= 18 && edad < 50) {
             System.out.print("Adulto");
+            } else {
+                System.out.println("Adulto mayor");
+            }
         } else {
-            System.out.println("Adulto mayor");
+            System.out.println("Edad no válida");
         }
     }
 }
